651abbfcdb4064de06d4ca8008bd9b396936f228
xyegithub <xye@bupt.edu.cn>
2022-04-09 10:02:08 +0800

    new

--- create_diary.fish
+++ create_diary.fish
@@ -2,7 +2,7 @@ if not test -e ~/myDiary/diary/$(date +"%Y-%m-%d").md
     # if not test -e /home/yexiang/myDiary/diary/s.md
     set old_file (ls -l ~/myDiary/diary/ | awk '{print $9}' | sort | tail -n -1)
     if test $old_file
-        cp ~/myDiary/diary/$old_file ~/mynote/diary/$(date +"%Y-%m-%d").md
+        cp ~/myDiary/diary/$old_file ~/myDiary/diary/$(date +"%Y-%m-%d").md
     end
 end
 vi ~/myDiary/diary/$(date +"%Y-%m-%d").md
