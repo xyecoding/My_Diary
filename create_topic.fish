d1fe108c3b7621b0a53b13bfa6dd1eb72ea63bea
xyegithub <xye@bupt.edu.cn>
2022-09-26 16:54:01 +0800

    new

--- create_topic.fish
+++ create_topic.fish
@@ -1,6 +1,8 @@
-read -l -p "set_color brcyan; echo '🔴 The name of the topic? [For example, peace_and_love or peach and love] ' ; set_color normal; echo '> '" confirm
+set_color red; echo "Topics Already Exist:"
+set_color yellow; ls ~/myDiary/topics/
+read -l -p "set_color cyan; echo '🔴 The name of the topic?  [For example, peace_and_love or peach and love] ' ; set_color normal; echo '> '" confirm
 set filename $(date +"%Y-%m-%d")_$confirm.md
 
 set filename_new (echo $filename | string replace -a _ ' ' | sed 's/^\w\|\s\w/\U&/g' | string replace -a ' ' _)
 
-vi ~/myDiary/topics/$filename_new
+# vi ~/myDiary/topics/$filename_new
